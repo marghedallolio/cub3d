5fb7e161ff29693f7f2f000e2aff807ee116ecc3
Francesco Martini <francema@c1r5p8.42firenze.it>
2025-11-18 16:52:17 +0100

    init_ray added

--- raycasting/raycasting.c
+++ raycasting/raycasting.c
@@ -6,11 +6,56 @@
 /*   By: francema <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 11:03:34 by francema          #+#    #+#             */
-/*   Updated: 2025/11/11 11:07:27 by francema         ###   ########.fr       */
+/*   Updated: 2025/11/18 16:49:54 by francema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cube3d.h"
+
+void	init_ray(t_ray *ray, t_player *p, int x)
+{
+	double camera_x;
+
+	// Normalizza X in coordinate camera da -1 a 1
+	camera_x = 2 * x / (double)SCREEN_W - 1.0;
+
+	// Direzione del raggio
+	ray->ray_dir_x = p->dir_x + p->plane_x * camera_x;
+	ray->ray_dir_y = p->dir_y + p->plane_y * camera_x;
+
+	// In quale cella della mappa si trova il giocatore
+	ray->map_x = (int)p->pos_x;
+	ray->map_y = (int)p->pos_y;
+
+	// Lunghezza dei raggi dal punto a passo successivo in X/Y
+	ray->delta_dist_x = (ray->ray_dir_x == 0) ? 1e30 : fabs(1 / ray->ray_dir_x);
+	ray->delta_dist_y = (ray->ray_dir_y == 0) ? 1e30 : fabs(1 / ray->ray_dir_y);
+
+	// Setup variabili DDA
+	ray->hit = 0;
+
+	// prepare steps
+	if (ray->ray_dir_x < 0)
+	{
+		ray->step_x = -1;
+		ray->side_dist_x = (p->pos_x - ray->map_x) * ray->delta_dist_x;
+	}
+	else
+	{
+		ray->step_x = 1;
+		ray->side_dist_x = (ray->map_x + 1.0 - p->pos_x) * ray->delta_dist_x;
+	}
+	if (ray->ray_dir_y < 0)
+	{
+		ray->step_y = -1;
+		ray->side_dist_y = (p->pos_y - ray->map_y) * ray->delta_dist_y;
+	}
+	else
+	{
+		ray->step_y = 1;
+		ray->side_dist_y = (ray->map_y + 1.0 - p->pos_y) * ray->delta_dist_y;
+	}
+}
 
 void	raycast(t_game *game)
 {
